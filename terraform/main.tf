70539d3706ccc0b6b75d5e72b277fe369cc93e70
jf3079 <jerome3079@gmail.com>
2024-01-10 16:35:08 -0500

    added dev3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 module "dev1" {
   source = "./modules/aft-account-request"
-
   control_tower_parameters = {
     AccountEmail              = "jerome3079+d1@gmail.com"
     AccountName               = "Dev1-Dev1"
@@ -9,26 +8,21 @@ module "dev1" {
     SSOUserFirstName          = "Dev1"
     SSOUserLastName           = "Dev1"
   }
-
   account_tags = {
     "Learn Tutorial" = "AFT"
   }
-
   change_management_parameters = {
     change_requested_by = "Jerome"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
-
   custom_fields = {
     group = "non-prod"
   }
-
   account_customizations_name = "dev1"
 }
 
 module "dev2" {
   source = "./modules/aft-account-request"
-
   control_tower_parameters = {
     AccountEmail              = "jerome3079+d2@gmail.com"
     AccountName               = "Dev2-Dev2"
@@ -37,19 +31,38 @@ module "dev2" {
     SSOUserFirstName          = "Dev2"
     SSOUserLastName           = "Dev2"
   }
-
   account_tags = {
     "Learn Tutorial" = "AFT"
   }
-
   change_management_parameters = {
     change_requested_by = "Jerome"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
-
   custom_fields = {
     group = "non-prod"
   }
-
   account_customizations_name = "dev2"
+}
+
+module "dev3" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "jerome3079+d3@gmail.com"
+    AccountName               = "Dev3-Dev3"
+    ManagedOrganizationalUnit = "Developers"
+    SSOUserEmail              = "jerome3079+d3@gmail.com"
+    SSOUserFirstName          = "Dev3"
+    SSOUserLastName           = "Dev3"
+  }
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "Jerome"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+  custom_fields = {
+    group = "non-prod"
+  }
+  account_customizations_name = "dev3"
 }
