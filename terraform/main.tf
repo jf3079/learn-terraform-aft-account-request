062dde792a51b701ef80be482468418920bc7150
jf3079 <jerome3079@gmail.com>
2024-01-11 10:51:35 -0500

    added 2 more accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,3 +44,48 @@ module "dev02" {
   account_customizations_name = "dev02"
 }
 
+module "dev03" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "jerome3079+d03@gmail.com"
+    AccountName               = "Dev-03"
+    ManagedOrganizationalUnit = "Developers"
+    SSOUserEmail              = "jerome3079+d03@gmail.com"
+    SSOUserFirstName          = "Dev03"
+    SSOUserLastName           = "Dev03"
+  }
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "Jerome"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+  custom_fields = {
+    group = "non-prod"
+  }
+  account_customizations_name = "dev03"
+}
+
+module "dev04" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "jerome3079+d04@gmail.com"
+    AccountName               = "Dev-04"
+    ManagedOrganizationalUnit = "Developers"
+    SSOUserEmail              = "jerome3079+d04@gmail.com"
+    SSOUserFirstName          = "Dev04"
+    SSOUserLastName           = "Dev04"
+  }
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "Jerome"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+  custom_fields = {
+    group = "non-prod"
+  }
+  account_customizations_name = "dev04"
+}
