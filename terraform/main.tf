8f7e5fdfedfeec4861eb8d29d4b52382715ceea0
jf3079 <jerome3079@gmail.com>
2024-01-11 14:04:37 -0500

    updated account request

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
-module "dev01" {
+module "dev001" {
   source = "./modules/aft-account-request"
   control_tower_parameters = {
-    AccountEmail              = "jerome3079+d01@gmail.com"
-    AccountName               = "Dev-01"
+    AccountEmail              = "jerome3079+d001@gmail.com"
+    AccountName               = "Dev-001"
     ManagedOrganizationalUnit = "Developers"
-    SSOUserEmail              = "jerome3079+d01@gmail.com"
-    SSOUserFirstName          = "Dev01"
-    SSOUserLastName           = "Dev01"
+    SSOUserEmail              = "jerome3079+d001@gmail.com"
+    SSOUserFirstName          = "Dev001"
+    SSOUserLastName           = "Dev001"
   }
   account_tags = {
     "Learn Tutorial" = "AFT"
@@ -21,71 +21,3 @@ module "dev01" {
   account_customizations_name = "sandbox"
 }
 
-module "dev02" {
-  source = "./modules/aft-account-request"
-  control_tower_parameters = {
-    AccountEmail              = "jerome3079+d02@gmail.com"
-    AccountName               = "Dev-02"
-    ManagedOrganizationalUnit = "Developers"
-    SSOUserEmail              = "jerome3079+d02@gmail.com"
-    SSOUserFirstName          = "Dev02"
-    SSOUserLastName           = "Dev02"
-  }
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-  change_management_parameters = {
-    change_requested_by = "Jerome"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-  custom_fields = {
-    group = "non-prod"
-  }
-  account_customizations_name = "sandbox"
-}
-
-module "dev03" {
-  source = "./modules/aft-account-request"
-  control_tower_parameters = {
-    AccountEmail              = "jerome3079+d03@gmail.com"
-    AccountName               = "Dev-03"
-    ManagedOrganizationalUnit = "Developers"
-    SSOUserEmail              = "jerome3079+d03@gmail.com"
-    SSOUserFirstName          = "Dev03"
-    SSOUserLastName           = "Dev03"
-  }
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-  change_management_parameters = {
-    change_requested_by = "Jerome"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-  custom_fields = {
-    group = "non-prod"
-  }
-  account_customizations_name = "sandbox"
-}
-
-module "dev04" {
-  source = "./modules/aft-account-request"
-  control_tower_parameters = {
-    AccountEmail              = "jerome3079+d04@gmail.com"
-    AccountName               = "Dev-04"
-    ManagedOrganizationalUnit = "Developers"
-    SSOUserEmail              = "jerome3079+d04@gmail.com"
-    SSOUserFirstName          = "Dev04"
-    SSOUserLastName           = "Dev04"
-  }
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-  change_management_parameters = {
-    change_requested_by = "Jerome"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-  custom_fields = {
-    group = "non-prod"
-  }
-  account_customizations_name = "sandbox"
-}
